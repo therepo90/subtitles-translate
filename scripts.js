6713e5425935fd23c7ce9e93e5bdb7d7665dff7a
therepo90 <therepo90@gmail.com>
2024-04-22 15:19:01 +0200

    x

--- scripts.js
+++ scripts.js
@@ -6,8 +6,11 @@ document.addEventListener("DOMContentLoaded", function() {
     const filenamePreview = document.getElementById('filename-preview');
     const uploadText = document.getElementById('upload-text');
     const step2 = document.getElementById('step2');
+    const step3 = document.getElementById('step3');
     const loading = document.getElementById('loading');
     const submitButton = document.getElementById('submit');
+    const calcCost = document.getElementById('calc-cost');
+    const costPreview = document.getElementById('cost-preview');
     // sprawdz czy sa wszystkie el, a jak nie ma to zglos ktory
     if (!dropzone || !fileInput || !filenamePreview || !uploadText || !step2 || !submitButton) {
         console.error('Brakuje elementów na stronie!');
@@ -64,23 +67,77 @@ document.addEventListener("DOMContentLoaded", function() {
     }
 
     // Obsługa przesyłania pliku i tłumaczenia
-    submitButton.addEventListener('click', async () => {
-        step2.classList.add('hidden');
-        loading.classList.remove('hidden');
+    calcCost.addEventListener('click', async () => {
+        const formData = prepareInput();
+        try {
 
+            const response = await fetch(`${host}/check-file`, {
+                method: 'POST',
+                body: formData
+            });
+            await checkResError(response);
+
+            const cost = await response.text();
+            console.log(cost);
+            if(cost ==='free'){
+                costPreview.textContent = `You are lucky. Its free.`;
+            }else {
+                // costPreview.textContent = `Cost: ${cost}€`;
+                costPreview.textContent = `Quota not available. Come back next month or email me.`;
+            }
+
+            if(cost === 'free') {
+                step3.classList.remove('hidden');
+            }
+        } catch (error) {
+            handleResError(error);
+        }
+    });
+    const prepareInput = () => {
         const file = fileInput.files[fileInput.files.length - 1];
         console.log({fileInput})
         console.log({file})
+        const formData = new FormData();
+        formData.append('file', file);
+        return formData;
+    }
+    const checkResError = async (response) => {
+
+        if (!response.ok) {
+            // if 429 say to come back later
+            if(response.status === 429) {
+                const msg = 'Too many requests. Please try again in 2 hours.'
+                alert(msg);
+                throw new Error(msg);
+            }
+            let errorMessage = 'Error';
+            try {
+                const errorResponse = await response.json();
+                if (errorResponse && errorResponse.message) {
+                    errorMessage = errorResponse.message;
+                }
+            } catch (error) {
+                console.error('Error', error);
+            }
+            throw new Error(errorMessage);
+        }
+    }
+    const handleResError = (error) => {
+        alert(error);
+        console.error('Error:');
+        console.error(error);//
+    }
+    submitButton.addEventListener('click', async () => {
+        step2.classList.add('hidden');
+        loading.classList.remove('hidden');
+        const formData = prepareInput();
         const targetLanguageInput = document.getElementById('target-language');
         const targetLanguage = targetLanguageInput.value; // Pobierz wartość z pola target-language
-
         // Sprawdź, czy pole target-language nie jest puste
         if (!targetLanguage) {
             alert('Please enter the target language code.');
             return;
         }
-        const formData = new FormData();
-        formData.append('file', file);
         formData.append('targetLanguage', targetLanguage); // Dodaj wartość target-language do danych formularza
 
         // clear input and hide step2
@@ -90,25 +147,7 @@ document.addEventListener("DOMContentLoaded", function() {
                 method: 'POST',
                 body: formData
             });
-
-            if (!response.ok) {
-                // if 429 say to come back later
-                if(response.status === 429) {
-                    const msg = 'Too many requests. Please try again in 2 hours.'
-                    alert(msg);
-                    throw new Error(msg);
-                }
-                let errorMessage = 'Error';
-                try {
-                    const errorResponse = await response.json();
-                    if (errorResponse && errorResponse.message) {
-                        errorMessage = errorResponse.message;
-                    }
-                } catch (error) {
-                    console.error('Error', error);
-                }
-                throw new Error(errorMessage);
-            }
+            await checkResError(response);
 
             const fileId = await response.text();
             // Tutaj możesz obsłużyć przetłumaczony tekst, np. wyświetlić go na stronie
@@ -116,9 +155,7 @@ document.addEventListener("DOMContentLoaded", function() {
             window.open(`${host}/file/${fileId}`);
 
         } catch (error) {
-            alert(error);
-            console.error('Error:');
-            console.error(error);//
+            handleResError(error);
         }
         step2.classList.add('hidden');
         loading.classList.add('hidden');
